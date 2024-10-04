ebe9b96dbab6d6e0e7b651d0bf69c81f89f78809
Boonrit-D <boonrit.du14@gmail.com>
2024-10-04 18:24:23 +0700

    Update log in and register page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
@@ -6,10 +6,9 @@ import { AuthService } from '../../service/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(
@@ -17,23 +16,44 @@ export class RegisterComponent {
     private router: Router,
     private ngZone: NgZone,
     private authService: AuthService
-  ){
+  ) {
     this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
     });
   }
 
-  onSubmit(): any {
-    this.authService.register(this.registerForm.value).subscribe({
-      next: () => {
-        console.log('User registered');
-        this.ngZone.run(() => this.router.navigateByUrl('/dashboard')); // แก้ไขเป็น /login หลังจากสร้างหน้า login สำเร็จแล้ว
-      },
-      error: (err) =>{
-        console.log(err);
-      }
-    })
+  ngOnInit(): void {}
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
   }
 
+  onSubmit(): any {
+    if (
+      this.registerForm.valid &&
+      !this.passwordMatchValidator(this.registerForm)
+    ) {
+      const { username, password } = this.registerForm.value;
+      const dataToSend = { username, password };
+      console.log(dataToSend);
+      this.authService.register(dataToSend).subscribe({
+        next: () => {
+          alert('ลงทะเบียนสำเร็จ');
+          console.log('User registered');
+          this.ngZone.run(() => this.router.navigateByUrl('/dashboard')); // แก้ไขเป็น /login หลังจากสร้างหน้า login สำเร็จแล้ว
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+      console.log(this.registerForm.value);
+    } else {
+      alert('รหัสผ่านไม่ตรงกัน กรุณาตรวจสอบอีกครั้ง');
+      console.log('Form is invalid');
+    }
+  }
 }
