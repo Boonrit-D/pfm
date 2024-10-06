961305ba85bb4c4fab95cdff339ec346ae308b8e
Boonrit-D <boonrit.du14@gmail.com>
2024-10-06 12:21:24 +0700

    Create PIN Code button

--- src/app/header-bar/header-bar.component.ts
+++ src/app/header-bar/header-bar.component.ts
@@ -12,8 +12,13 @@ export class HeaderBarComponent {
   // ใช้เพื่อควบคุมการแสดงผล
   menuVisible = false;
   menuBarVisible = false;
-  isCollapsed = false;
   isDropdownOpen = false;
+  showPopover = false;
+  showPinLogin = false;
+  
+  pinCode: string = ''; // เก็บรหัส PIN ที่ผู้ใช้ป้อน
+  pinDisplay: string[] = ['', '', '', '']; // แสดง PIN ที่กรอก
+  numbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#']; // ตัวเลขบนแป้นกด
 
   // ประกาศตัวแปร authService
   constructor(public authService: AuthService, private router: Router) {}
@@ -42,5 +47,35 @@ export class HeaderBarComponent {
 
   toggleMenuBar() {
     this.menuBarVisible = !this.menuBarVisible; // เปลี่ยนค่าเมื่อคลิก
+  }
+
+  closePinLogin() {
+    this.showPinLogin = false;
+    this.pinCode = '';
+    this.pinDisplay = ['', '', '', ''];
+  }
+
+  enterPin(number: string) {
+    if (this.pinCode.length < 4) {
+      this.pinCode += number;
+      this.pinDisplay[this.pinCode.length - 1] = number;
+    }
+  }
+
+  deletePin() {
+    if (this.pinCode.length > 0) {
+      this.pinDisplay[this.pinCode.length - 1] = '';
+      this.pinCode = this.pinCode.slice(0, -1);
+    }
+  }
+
+  loginWithPin() {
+    if (this.pinCode.length === 4) {
+      // ทำการเข้าสู่ระบบด้วยรหัส PIN
+      console.log('PIN:', this.pinCode);
+      this.closePinLogin(); // ปิด popover หลังจากเข้าสู่ระบบสำเร็จ
+    } else {
+      alert('Please enter a 4-digit PIN');
+    }
   }
 }
